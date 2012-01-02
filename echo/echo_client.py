70bba743dc531a7b7e91773b7e3e16dce31894dd
UW Student <jackyj@speakeasy.net>
2012-01-01 19:23:01 -0800

    add echo directory with README, server, client

---
+++ echo/echo_client.py
@@ -0,0 +1,30 @@
+"""
+echo client, usage:
+
+ python echo_client.py <host> <port>
+
+Both host and port are optional, defaults: localhost 50000
+host must be present if you want to provide port
+"""
+
+import socket 
+import sys
+
+host = 'localhost' 
+port = 50000 
+size = 1024 
+
+nargs = len(sys.argv)
+if nargs > 1:
+    host = sys.argv[1]
+if nargs > 2:
+    port = int(sys.argv[2])
+
+s = socket.socket(socket.AF_INET, 
+                  socket.SOCK_STREAM) 
+s.connect((host,port)) 
+s.send('Hello, world') 
+data = s.recv(size) 
+s.close() 
+print 'from (%s,%s): %s' % (host, port, data)
+
---
+++ echo/echo_server.py
@@ -0,0 +1,39 @@
+"""
+echo server, usage:
+
+ python echo_server.py <port>
+
+Port is optional, default: 50000
+"""
+
+import socket 
+import sys
+
+host = '' 
+port = 50000 
+
+if len(sys.argv) > 1:
+    port = int(sys.argv[1])
+
+backlog = 5 
+size = 1024 
+
+# server's listener socket
+s = socket.socket(socket.AF_INET, socket.SOCK_STREAM) 
+
+# Release listener socket immediately when program exits, 
+# avoid socket.error: [Errno 48] Address already in use
+s.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
+
+s.bind((host,port)) 
+
+print 'echo_server listening on port', port
+s.listen(backlog) 
+
+while True: 
+    client, address = s.accept()
+    data = client.recv(size) 
+    if data: 
+        client.send(data) 
+    print 'from %s: %s' % (address, data)
+    client.close()
